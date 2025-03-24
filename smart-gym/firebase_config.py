60c518be8b6ac468c544d10defb78e0446497596
Jodie Ong <113889432+OWSJodie@users.noreply.github.com>
2025-03-24 17:06:07 +0800

    Clean up
    Plan :
    Shifting content out from js script
    removing get-scan content
    
    adding sessionManager using google auth

--- smart-gym/firebase_config.py
+++ smart-gym/firebase_config.py
@@ -1,21 +1,12 @@
 import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore, auth as admin_auth
-
-# ✅ Firebase Config (from Firebase Console)
-firebase_config = {
-    "apiKey": "AIzaSyBmdF2vevfws4P3YMPbgqM_mYhKXN3fQJY",
-    "authDomain": "smartgymbuddy-4b2b9.firebaseapp.com",
-    "databaseURL": "https://smartgymbuddy-4b2b9-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "smartgymbuddy-4b2b9",
-    "storageBucket": "smartgymbuddy-4b2b9.firebasestorage.app",
-    "messagingSenderId": "1022492759891",
-    "appId": "1:1022492759891:web:e00fdd0bddce44d802b3a6",
-    "measurementId": "G-C1DVLC7LB2"
-}
+import json
 
 # ✅ Initialize Firebase Auth (Pyrebase for client-side auth)
-firebase = pyrebase.initialize_app(firebase_config)
+with open('firebase-auth.json') as f:
+    config = json.load(f)
+firebase = pyrebase.initialize_app(config)
 client_auth = firebase.auth()
 
 # ✅ Initialize Firestore and Admin SDK (for server-side actions)
