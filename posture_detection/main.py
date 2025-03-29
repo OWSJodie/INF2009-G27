db73a5b9b3c0ddb28f18d66dffe8a6b71582413a
Jodie Ong <113889432+OWSJodie@users.noreply.github.com>
2025-03-30 00:59:17 +0800

    Added try-catch
    To prevent crash if rfid fail to read.

--- posture_detection/main.py
+++ posture_detection/main.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     print("[SYSTEM] Starting RFID thread...")
-    start_rfid_thread()
+    try:
+        print("[SYSTEM] Starting RFID thread...")
+        start_rfid_thread()
+    except Exception as e:
+        print(f"[ERROR] Failed to start RFID thread: {e}")
 
     try:
         print("[SYSTEM] Starting video stream...")
