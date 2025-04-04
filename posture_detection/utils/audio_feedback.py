01edf8ad5c184720d21abda2e5ca97537e30db25
Ong Wei Song Jodie <113889432+OWSJodie@users.noreply.github.com>
2025-04-04 15:50:45 +0800

    fix issue where the audio keep creating multiple threads

--- posture_detection/utils/audio_feedback.py
+++ posture_detection/utils/audio_feedback.py
@@ -4,19 +4,30 @@ import time
 import os
 
 pygame.mixer.init()
-time.sleep(0.1)  # Let the mixer fully init
+time.sleep(0.1)
+
+is_playing = False
+play_lock = threading.Lock()
 
 def play_sound(file_path):
     def _play():
-        if not os.path.exists(file_path):
-            print(f"File not found: {file_path}")
-            return
-        sound = pygame.mixer.Sound(file_path)
-        sound.play()
-        while pygame.mixer.get_busy():
-            time.sleep(0.1)
-    threading.Thread(target=_play, daemon=True).start()
+        global is_playing
+        with play_lock:  # Lock so only one thread plays at a time
+            if is_playing:
+                return  # Another thread is playing sound
+            if not os.path.exists(file_path):
+                print(f"File not found: {file_path}")
+                return
+            is_playing = True
+            sound = pygame.mixer.Sound(file_path)
+            sound.play()
+            print(f"Playing Audio {file_path}")
+            while pygame.mixer.get_busy():
+                time.sleep(0.1)
+            is_playing = False
+            print(f"Finished playing {file_path}")
 
+    threading.Thread(target=_play, daemon=True).start()
 
 if __name__ == "__main__":
     # Play sound
